d60059a1b5ea4ddbb48a42ed1f79be823ebba8ae
Sergio Cabral <sergio@gohorse.dev>
2023-03-13 20:23:46 -0300

    feat: níveis de log

--- Program.cs
+++ Program.cs
@@ -3,7 +3,13 @@
 Console.WriteLine("Hello, World!");
 
 var loggerConfiguration = new LoggerConfiguration();
+loggerConfiguration.MinimumLevel.Verbose();
 loggerConfiguration.WriteTo.Console();
 Log.Logger = loggerConfiguration.CreateLogger();
 
-Log.Information("Hello, Logger!");
+Log.Verbose("Informações muito detalhadas.");
+Log.Debug("Informações de depuração.");
+Log.Information("Informações gerais.");
+Log.Warning("Não são erros, mas podem podem levar a problemas no futuro.");
+Log.Error("Erros que impedem o funcionamento correto.");
+Log.Fatal("Erros graves que fazem o aplicativo parar de funcionar.");
